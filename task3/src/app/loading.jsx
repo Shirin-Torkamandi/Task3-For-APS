d332c28da0303a84e58d860e1d0603192578320f
Shirin-Torkamandi <shirin.t44@gmail.com>
2024-09-25 01:09:08 +0330

    make the circle to go in the center

--- task3/src/app/loading.jsx
+++ task3/src/app/loading.jsx
@@ -1,9 +1,17 @@
 import * as React from "react";
+import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Loading() {
   return (
-    <React.Fragment>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
       <svg width={0} height={0}>
         <defs>
           <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
@@ -15,6 +23,6 @@ export default function Loading() {
       <CircularProgress
         sx={{ "svg circle": { stroke: "url(#my_gradient)" } }}
       />
-    </React.Fragment>
+    </Box>
   );
 }
